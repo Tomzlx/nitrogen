c4af353366652ec39f20547fc681be135e79fc83
Tomzlx <tomtm1401@gmail.com>
2021-04-03 16:26:59 +0100

    Update nitro.js

--- commands/nitro.js
+++ commands/nitro.js
@@ -10,7 +10,7 @@ module.exports = {
         const thisEmbed = new Discord.MessageEmbed()
         .setTitle('Your nitro gift has arrived!')
         .setDescription('Hey, thank you for using our generator!\n\nYour nitro gift is: https://discord.gift/94JEFUEFC8235J\n\nIf you have any problems, contact a server administrator!')
-        .setThumbnail('https://media.discordapp.net/attachments/817483898144686133/817790924242681896/c62fdaf57b49b5bbaa4138c2eac49da1_large-removebg-preview.png')
+        .setThumbnail('https://images-ext-1.discordapp.net/external/wJx7Ln0o5QQvE3Ii9X5GwOdZJpjPyXNpjCatJN_H-Nc/https/distok.top/stickers/749043879713701898/749052505308266645.gif')
 
         message.author.send(thisEmbed);
     }
